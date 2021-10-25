6958cb671495f3164ba9aa3722365b6aa0d065f3
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-10-25 20:46:25 +0800

    将结果保存为json字符串，存入文档

--- mitmproxy_test.py
+++ mitmproxy_test.py
@@ -1,7 +1,5 @@
 from mitmproxy import http
 import json
-import openpyxl
-
 # def request(flow):
 #     url = "http://www.gsosc.cn/prod-api/app/dt/"
 #     # if flow.request.method == "POST":
@@ -17,15 +15,19 @@ import openpyxl
 def response(flow) -> dict:
     url = "http://www.gsosc.cn/prod-api/app/dt/"
     exam_url = "http://www.gsosc.cn/prod-api/app/bzCeShi/loadCsTiByDtId"
-    if exam_url in flow.request.url:
+    # if exam_url in flow.request.url:
+    if url in flow.request.url:
         print(flow.response.text)
         results = json.loads(flow.response.text)
         datas: list = results.get("data")
-        for data in datas:
-            quetions = {}
-            quetions['title'] = data.get("timu")
-            quetions['analyze'] = data.get("jieXi").replace("<br>", "")
+        with open("ccc.txt",'w',encoding='utf8') as f:
+            for data in datas:
+                quetions = {}
+                quetions['title'] = data.get("timu")
+                quetions['analyze'] = data.get("jieXi").replace("<br>", "")
 
-            for option in data.get("options"):
-                quetions[option.get("bianHao")] = option.get("options")
-            print(quetions)
+                for option in data.get("options"):
+                    quetions[option.get("bianHao")] = option.get("options")
+                # print(str(quetions))
+                tap = json.dumps(quetions,ensure_ascii=False)
+                f.write(tap)
