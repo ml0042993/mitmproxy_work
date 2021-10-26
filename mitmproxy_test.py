f86f78e557129862665a9ad62ed9a7bf1bbbb6be
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-10-26 15:24:38 +0800

    完成显示

--- mitmproxy_test.py
+++ mitmproxy_test.py
@@ -16,18 +16,23 @@ def response(flow) -> dict:
     url = "http://www.gsosc.cn/prod-api/app/dt/"
     exam_url = "http://www.gsosc.cn/prod-api/app/bzCeShi/loadCsTiByDtId"
     # if exam_url in flow.request.url:
-    if url in flow.request.url:
+    if exam_url in flow.request.url:
         print(flow.response.text)
         results = json.loads(flow.response.text)
         datas: list = results.get("data")
-        with open("ccc.txt",'w',encoding='utf8') as f:
+        with open("info.txt",'w',encoding='utf8') as f:
+            file = []
             for data in datas:
                 quetions = {}
+                options = []
                 quetions['title'] = data.get("timu")
                 quetions['analyze'] = data.get("jieXi").replace("<br>", "")
 
                 for option in data.get("options"):
-                    quetions[option.get("bianHao")] = option.get("options")
+                    # quetions["options"] = option.get("options")
+                    options.append(option.get("bianHao")+":"+option.get("options"))
+                quetions["options"] = options
                 # print(str(quetions))
-                tap = json.dumps(quetions,ensure_ascii=False)
-                f.write(tap)
+                # tap :dict = quetions
+                file.append(quetions)
+            f.write(json.dumps(file,ensure_ascii=False))
---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+import json
+__all__ = ["app"]
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+def readfile():
+	with open('info.txt', 'r', encoding='utf8') as f:
+		return f.read()
+@app.route('/')
+def index():
+	messages = json.loads(readfile())
+	print(type(messages))
+	return render_template('index.html', messages=messages)
+
+
+if __name__ == '__main__':
+	app.run()
