f73c1e737a3b654a87cd3c09651fa7c65ea9eef4
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-11-03 16:09:23 +0800

    合并测试功能

--- mitmproxy_test.py
+++ mitmproxy_test.py
@@ -13,26 +13,64 @@ import json
 #         # print(flow.request.get_text)
 #         # print(flow.response.contents)
 def response(flow) -> dict:
-    url = "http://www.gsosc.cn/prod-api/app/dt/"
-    exam_url = "http://www.gsosc.cn/prod-api/app/bzCeShi/loadCsTiByDtId"
+    url = "http://yjpf.gsosc.cn/prod-api/app/bzCeShi/loadCsTiByDtId"
+    exam_url = "http://yjpf.gsosc.cn/prod-api/app/bzCeShi/loadCsTiByDtId"
+    wechat_url = 'https://shanxi.guangyusoft.com:12721/ShanXi3-3-5k/examsystem_getExamSystemQuestionById.action?uid='
     # if exam_url in flow.request.url:
-    if exam_url or url in flow.request.url:
-        print(flow.response.text)
-        results = json.loads(flow.response.text)
-        datas: list = results.get("data")
-        with open("info.txt",'w',encoding='utf8') as f:
-            file = []
-            for data in datas:
-                quetions = {}
-                options = []
-                quetions['title'] = data.get("timu")
-                quetions['analyze'] = data.get("jieXi").replace("<br>", "")
+    if exam_url in flow.request.url:
+        mian(flow)
+    elif url in flow.request.url:
+        mian(flow)
+    elif wechat_url in flow.request.url:
+        wechat(flow)
+def mian(flow):
+    print(flow.response.text)
+    results = json.loads(flow.response.text)
+    datas: list = results.get("data")
+    with open("info.txt", 'w', encoding='utf8') as f:
+        file = []
+        for data in datas:
+            quetions = {}
+            options = []
+            quetions['title'] = data.get("timu")
+            quetions['analyze'] = data.get("jieXi").replace("<br>", "")
 
-                for option in data.get("options"):
-                    # quetions["options"] = option.get("options")
-                    options.append(option.get("bianHao")+":"+option.get("options"))
-                quetions["options"] = options
-                # print(str(quetions))
-                # tap :dict = quetions
-                file.append(quetions)
-            f.write(json.dumps(file,ensure_ascii=False))
+            for option in data.get("options"):
+                # quetions["options"] = option.get("options")
+                options.append(option.get("bianHao") + ":" + option.get("options"))
+            quetions["options"] = options
+            # print(str(quetions))
+            # tap :dict = quetions
+            file.append(quetions)
+        f.write(json.dumps(file, ensure_ascii=False))
+def wechat(flow):
+    print(flow.response.text)
+    dict_responses = json.loads(flow.response.text)
+    dict_response = dict_responses.get('question')
+
+    que_info = {}
+    que_info['title'] = dict_response.get('content')
+    que_info['answer'] = dict_response.get('answer')
+    que_info['option'] = []
+    if dict_response.get('optionOne'):
+        que_info['option'].append("A." + dict_response.get('optionOne'))
+    if dict_response.get('optionTwo'):
+        que_info['option'].append('B.' + dict_response.get('optionTwo'))
+    if dict_response.get('optionThree'):
+        que_info['option'].append('C.' + dict_response.get('optionThree'))
+    if dict_response.get('optionFour'):
+        que_info['option'].append('D.' + dict_response.get('optionFour'))
+    if dict_response.get('optionFive'):
+        que_info['option'].append('E.' + dict_response.get('optionFive'))
+    if dict_response.get('optionSix'):
+        que_info['option'].append('F.' + dict_response.get('optionSix'))
+    if dict_response.get('optionSeven'):
+        que_info['option'].append('G.' + dict_response.get('optionSeven'))
+    if dict_response.get('optionEight'):
+        que_info['option'].append('H.' + dict_response.get('optionEight'))
+    if dict_response.get('optionNine'):
+        que_info['option'].append('I.' + dict_response.get('optionNine'))
+
+    print(que_info)
+    with open('we_info.txt', 'w', encoding='utf8') as f:
+        f.write(json.dumps(que_info, ensure_ascii=False))
--- run.py
+++ run.py
@@ -7,12 +7,19 @@ bootstrap = Bootstrap(app)
 def readfile():
 	with open('info.txt', 'r', encoding='utf8') as f:
 		return f.read()
+def we_readfile():
+	with open('we_info.txt', 'r', encoding='utf8') as f:
+		return f.read()
 @app.route('/')
 def index():
 	messages = json.loads(readfile())
 	print(type(messages))
 	return render_template('index.html', messages=messages)
-
+@app.route('/wechat')
+def wechat():
+	messages = json.loads(we_readfile())
+	print(type(messages))
+	return render_template('we_index.html', messages=messages)
 
 if __name__ == '__main__':
 	app.run(port=5555, debug=True)
